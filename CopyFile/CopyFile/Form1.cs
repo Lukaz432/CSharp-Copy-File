8a29224c6504391744534f2fd58835d8a28122e9
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-06-27 02:52:57 +0300

    Copy-File

---
+++ CopyFile/CopyFile/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyFile
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FileTestBtn_Click(object sender, EventArgs e)
+        {
+            string testFile = "C:\\Users\\NASA\\Desktop\\Test.txt";
+            DateTime dtCreated;
+            DateTime dtModified;
+
+            if (File.Exists(testFile))
+            {
+                textBox1.AppendText(testFile + " exists. Copying...\n");
+                File.Copy(testFile, testFile + "(Copy)", true);
+            }
+            else
+            {
+                textBox1.AppendText(testFile + " does not exist\n");
+            }
+            if (File.Exists(testFile))
+            {
+                dtCreated = File.GetCreationTime(testFile);
+                dtModified = File.GetLastWriteTime(testFile);
+                textBox1.AppendText(testFile + "was created at: " + dtCreated);
+                textBox1.AppendText(testFile + "was modified at: " + dtModified);
+            }
+        }
+    }
+}
